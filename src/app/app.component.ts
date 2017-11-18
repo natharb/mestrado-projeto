32f8152a79a5bb38cc969671ac7be04ffc498f58
nathalia <nathaliaarb@gmail.com>
2017-11-18 16:24:02 -0200

    funcionando 100%

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ElementRef } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators} from "@angular/forms";
-import { Http,Response,Headers,RequestOptions, URLSearchParams } from "@angular/http";
+import { FormGroup, FormControl, FormBuilder, Validators } from "@angular/forms";
+import { Http, Response, Headers, RequestOptions, URLSearchParams } from "@angular/http";
 import { AngularFireAuth } from 'angularfire2/auth'
 import { AngularFireDatabase } from 'angularfire2/database'
 import * as firebase from 'firebase/app'
 import 'firebase/storage'
 //import {UserData} from './user.model';
 import { Observable } from "rxjs";
-var $:any;
+var $: any;
 declare var recorderObject: any;
-declare function startRecording(button) : void;
-declare function stopRecording(button, id) : void;
+declare function startRecording(button): void;
+declare function stopRecording(button, id): void;
 
 @Component({
   selector: 'app-root',
@@ -20,29 +20,56 @@ declare function stopRecording(button, id) : void;
 })
 export class AppComponent implements OnInit {
 
-  isOn:boolean;
-  isOff:boolean;
+  isOn: boolean;
+  isOff: boolean;
 
-  userForm : FormGroup;
+  fraseOn: boolean;
+  fraseOff: boolean;
+
+  currentPercentage: String;
+
+  userForm: FormGroup;
 
   constructor(fb: FormBuilder) {
     this.userForm = fb.group({
-     'idade': [null, Validators.required],
-     'estado': [null, Validators.required],
-    'cidade': [null, Validators.required],
-    'sexo': [null, Validators.required],
-    'escolaridade': '' ,
-    'data': ''      
+      'idade': [null, Validators.required],
+      'estado': [null, Validators.required],
+      'cidade': [null, Validators.required],
+      'sexo': [null, Validators.required],
+      'escolaridade': '',
+      'data': ''
     })
-  }
-  
-  IsHidden= true;
-  onSelect(){
-   this.IsHidden = !this.IsHidden;
+
+    this.currentPercentage = '60';
   }
 
-  onSubmit(value: any): void {  
-    console.log('you submitted value:', value);  
+  sampleMethode(id) {
+    if (id == 'frase1') {
+      this.currentPercentage = '33.33';
+    } else if (status === 'inprogress') {
+      this.currentPercentage = '66.66';
+    } else {
+      this.currentPercentage = '100';
+    }
+  }
+
+  IsHidden = true;
+  onSelect() {
+    this.IsHidden = !this.IsHidden;
+  }
+
+
+  onSelectFrase(showHideDiv) {
+    for (let i = 1; i <= 17; i++) {
+      if (showHideDiv == 'frase' + i) {
+        document.getElementById("frase" + i).style.display = "none";
+        document.getElementById("frase" + (i + 1)).style.display = "block";
+      }
+    }
+  }
+
+  onSubmit(value: any): void {
+    console.log('you submitted value:', value);
   }
 
   ngOnInit() {
@@ -51,18 +78,16 @@ export class AppComponent implements OnInit {
     recorderObject.recorder();
   }
 
-  public start(button){
+  public start(button) {
     startRecording(button);
     this.isOn = true;
     this.isOff = false;
   }
 
-  public stop(id, button){
+  public stop(id, button) {
     stopRecording(button, id);
     this.isOn = false;
     this.isOff = true;
   }
-
-
 }
 
